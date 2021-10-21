3f3d5cdfcca3ffa8a25cc98a3b8530964b1b4a24
amnahkhatun <amna.khatun1605@gmail.com>
2021-10-21 12:56:54 +0530

    added animation in button

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -4,7 +4,7 @@ import "./Button.css";
 export default function Button({ text, className, href, newTab }) {
   return (
     <div className={className}>
-      <a class="main-button" href={href} target={newTab && "_blank"}>
+      <a class="main-button btn btn--white btn--animated" href={href} target={newTab && "_blank"}>
         {text}
       </a>
     </div>
